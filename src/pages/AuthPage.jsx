2f71b61ef167d0fa8d000c7c816c9f5a4c852d79
dilankasltl <savindudilanka9793@gmail.com>
2026-09-16 13:30:10 +0530

    defult remove😒

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -259,7 +259,7 @@ export default function AuthPage({ onLoginSuccess }) {
             </button>
 
             <div style={{ textAlign: 'center', fontSize: '0.75rem', color: 'var(--text-dim)', marginTop: '0.5rem' }}>
-              Default Admin: <strong></strong> | Password: <strong></strong>
+              <strong></strong>  <strong></strong>
             </div>
           </form>
         )}
